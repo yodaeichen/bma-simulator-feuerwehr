f386aa4cb93977a3a405f09ef65a9360a394028b
yodaeichen <marco@bender-eichen.de>
2026-01-26 17:56:14 +0100

    Update objects.js

--- server/data/objects.js
+++ server/data/objects.js
@@ -1,19 +1,20 @@
-module.exports = {
-  schule: {
-    id: "schule",
-    name: "Schule Musterstadt",
-    address: "Musterstraße 1, 12345 Musterstadt",
-    floors: {
-      EG: {
-        name: "Erdgeschoss",
-        detectors: {
-          "EG-01": {
-            name: "Flur Eingang",
-            type: "Rauchmelder",
-            route: "BMZ → Eingang → Flur"
-          }
+{
+  "objekt1": {
+    "name": "Grundschule Musterstadt",
+    "floors": {
+      "eg": {
+        "name": "Erdgeschoss",
+        "detectors": {
+          "m01": { "type": "Rauchmelder", "room": "Flur" },
+          "m02": { "type": "Rauchmelder", "room": "Klasse 1" }
+        }
+      },
+      "og1": {
+        "name": "1. Obergeschoss",
+        "detectors": {
+          "m03": { "type": "Handmelder", "room": "Treppenhaus" }
         }
       }
     }
   }
-};
+}
